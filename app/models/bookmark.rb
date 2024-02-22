7775f879614ba4eb0f935ac042ba431017bd3e70
clothilde scache <clothilde.sca@gmail.com>
2024-02-22 16:34:06 +0100

    added routes, controllers and views

---
+++ app/models/bookmark.rb
@@ -0,0 +1,6 @@
+class Bookmark < ApplicationRecord
+  belongs_to :movie
+  belongs_to :list
+  validates :comment, length: { minimum: 6, message: "Comment cannot be shorter than 6 characters" }
+  validates :movie_id, uniqueness: { scope: :list_id, message: "Bookmark already exists" }
+end
