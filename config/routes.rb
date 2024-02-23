eaffef380dd36252cc621ecd4b0920714539c76e
clothilde scache <clothilde.sca@gmail.com>
2024-02-23 16:54:13 +0100

    added style to my app

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
 
-  resources :lists, only: %i[index show new create] do
+  resources :lists, only: %i[index show new create destroy] do
     resources :bookmarks, only: %i[new create]
   end
   resources :bookmarks, only: %i[destroy]
